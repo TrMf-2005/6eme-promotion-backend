8dcd013b73edf6604a2df386000f8c8a6ce4dee7
TCHIBOZO Régis Maël F. <tchibozoregismael@gmail.com>
2026-09-24 18:43:02 +0100

    Add files via upload

---
+++ routes/publicRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const { verifySiteAccess } = require("../middleware/auth");
+const memberController = require("../controllers/memberController");
+const settingsController = require("../controllers/settingsController");
+
+router.get("/site-info", settingsController.getPublicSiteInfo);
+router.post("/members/submit", verifySiteAccess, memberController.submitMember);
+router.get("/members", verifySiteAccess, memberController.getValidatedMembers);
+
+module.exports = router;
