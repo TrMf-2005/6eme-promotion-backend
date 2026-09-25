4bb31c0535082dac3e28ec98b7a19b1f0acc30b6
TCHIBOZO Régis Maël F. <tchibozoregismael@gmail.com>
2026-09-25 16:41:17 +0100

    Update publicRoutes.js

--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -7,5 +7,6 @@ const settingsController = require("../controllers/settingsController");
 router.get("/site-info", settingsController.getPublicSiteInfo);
 router.post("/members/submit", verifySiteAccess, memberController.submitMember);
 router.get("/members", verifySiteAccess, memberController.getValidatedMembers);
+router.get("/members/:id", verifySiteAccess, memberController.getValidatedMemberDetail);
 
 module.exports = router;
