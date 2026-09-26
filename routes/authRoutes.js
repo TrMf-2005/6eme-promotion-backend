9cbeceea28a896a096c8c6573ee9493ad238aa0f
TCHIBOZO Régis Maël F. <tchibozoregismael@gmail.com>
2026-09-26 19:25:16 +0100

    Delete routes/authRoutes.js

--- routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authController = require("../controllers/authController");
-
-router.post("/public/verify-access", authController.verifySiteAccess);
-router.post("/admin/login", authController.adminLogin);
-router.post("/admin/set-password", authController.setInitialPassword);
-
-module.exports = router;
